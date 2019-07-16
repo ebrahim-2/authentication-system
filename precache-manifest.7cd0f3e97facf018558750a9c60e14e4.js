c1661950188bf08dce757dd9c7d7e36f20d5b495
EbrahimKreem <ebrahemkreem40@gmail.com>
2019-07-16 16:20:10 +0300

    Updates

---
+++ precache-manifest.7cd0f3e97facf018558750a9c60e14e4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a6e01004558b4b2ede3",
+    "url": "/authentication-system/static/js/6.4ed751c5.chunk.js"
+  },
+  {
+    "revision": "31a9c27fe2364734dd2c",
+    "url": "/authentication-system/static/js/0.20c34ae5.chunk.js"
+  },
+  {
+    "revision": "6ed0ad8e06eebc8be123",
+    "url": "/authentication-system/static/js/2.29a5ffe1.chunk.js"
+  },
+  {
+    "revision": "fb56e8d58e7b7907fd92",
+    "url": "/authentication-system/static/js/11.860fddd6.chunk.js"
+  },
+  {
+    "revision": "70f74260ac9c26dae394",
+    "url": "/authentication-system/static/js/main.5ac1ce93.chunk.js"
+  },
+  {
+    "revision": "a3b97b7a9a8b884dcf35",
+    "url": "/authentication-system/static/js/runtime~main.51f32823.js"
+  },
+  {
+    "revision": "a12891b1c9207cfedca2",
+    "url": "/authentication-system/static/js/5.d670d28b.chunk.js"
+  },
+  {
+    "revision": "fac8c6449a59313a58b2",
+    "url": "/authentication-system/static/js/1.af57c2ee.chunk.js"
+  },
+  {
+    "revision": "dde46f8ad1d82cc72009",
+    "url": "/authentication-system/static/js/7.b609a037.chunk.js"
+  },
+  {
+    "revision": "8cafd43234a3434d9b34",
+    "url": "/authentication-system/static/js/8.5d4cdfea.chunk.js"
+  },
+  {
+    "revision": "09351bf7bf3d2a6d4653",
+    "url": "/authentication-system/static/js/9.5d5fc800.chunk.js"
+  },
+  {
+    "revision": "6255f920b7adf33472f3",
+    "url": "/authentication-system/static/js/10.c161b9be.chunk.js"
+  },
+  {
+    "revision": "70f74260ac9c26dae394",
+    "url": "/authentication-system/static/css/main.11e03424.chunk.css"
+  },
+  {
+    "revision": "bf1490d7e82cfa2f125ed456916a2d14",
+    "url": "/authentication-system/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/authentication-system/precache-manifest.b2e5cf323a68e2463315dfc7d1f50f49.js"
+  "/authentication-system/precache-manifest.7cd0f3e97facf018558750a9c60e14e4.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/authentication-system/index.html"), {
+workbox.routing.registerNavigationRoute("/authentication-system/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
